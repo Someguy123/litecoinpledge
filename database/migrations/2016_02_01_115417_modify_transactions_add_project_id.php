78207d64eac0c924748d5d7ecdcb6fae24629c43
Chris (Someguy123) <info@someguy123.com>
2016-02-01 11:55:06 +0000

    forgot a migration

---
+++ database/migrations/2016_02_01_115417_modify_transactions_add_project_id.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ModifyTransactionsAddProjectId extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->integer('project_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->dropColumn('project_id');
+        });
+    }
+}
